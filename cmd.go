a3088958c9ad7786bb3f58f9a0d5e9883d926fd1
Roman Mazur <roman.mazur@intelity.com>
2019-04-21 11:10:06 +0300

    Add test implementation

---
+++ cmd.go
@@ -0,0 +1,38 @@
+package main // import "rmazur.io/hello"
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+var greetings = map[string]string{
+	"en": "Hello!",
+	"ua": "Вітаю!",
+	"fr": "Salut!",
+	"jp": "こんにちは",
+}
+
+func main() {
+
+	var lang string
+	if len(os.Args) > 1 {
+		lang = strings.ToLower(os.Args[1])
+		if _, know := greetings[lang]; !know {
+			fmt.Fprintf(os.Stderr, "I don't speak %s\n", lang)
+			os.Exit(1)
+		}
+	} else {
+		keys := make([]string, len(greetings))
+		i := 0
+		for k := range greetings {
+			keys[i] = k
+			i++
+		}
+
+		lang = keys[rand.Int31n(int32(len(keys)))]
+	}
+
+	fmt.Println(greetings[lang])
+}
